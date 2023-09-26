7d8158d4c1f7fb8d15227ef2d33bfe553a8095d6
Mikola Kutsko <kn212_kmm@student.ztu.edu.ua>
2023-09-26 22:21:29 +0300

    delete duplicated code

--- scripts/game.js
+++ scripts/game.js
@@ -9,7 +9,6 @@ class HangmanGame {
         this.messageElement = document.querySelector("#message");
         this.gameDiv = document.querySelector("#game");
         this.menuDiv = document.querySelector("#menu");
-
         this.word = "";
         this.guessedWord = "";
         this.attempt = MAXIMUM_NUMBERS_OF_ATTEMPTS;
@@ -28,98 +27,75 @@ class HangmanGame {
         this.menuDiv.classList.remove("hidden");
     }
 
-    updateAttempt() {
-        this.attemptsElement.innerHTML = `${this.attempt}/${MAXIMUM_NUMBERS_OF_ATTEMPTS}`;
-        this.attemptDemoElement.setAttribute(
-            "src",
-            `./images/Hangman-${MAXIMUM_NUMBERS_OF_ATTEMPTS - this.attempt}.png`
-        );
-    }
-
     guessWord(letter) {
         if (this.attempt < 1) {
             return;
         }
-    
+
         const lowercaseLetter = letter.toLowerCase();
         const lowercaseWord = this.word.toLowerCase();
-    
+
         if (lowercaseWord.includes(lowercaseLetter)) {
             this.handleCorrectGuess(lowercaseLetter);
         } else {
             this.handleIncorrectGuess();
         }
     }
-    
+
     handleCorrectGuess(letter) {
         const wordArray = this.word.split("");
         const guessedWordArray = this.guessedWord.split("");
         const letterIndices = [];
-    
+
         for (let i = 0; i < wordArray.length; i++) {
             if (wordArray[i].toLowerCase() === letter) {
                 letterIndices.push(i);
             }
         }
-    
+
         for (const index of letterIndices) {
             guessedWordArray[index] = wordArray[index];
         }
-    
+
         this.guessedWord = guessedWordArray.join("");
         this.wordElement.innerHTML = this.guessedWord;
-    
+
         if (this.guessedWord === this.word) {
             this.handleGameWin();
         } else {
             soundManager.playSuccess();
         }
     }
-    
-    
+
     handleIncorrectGuess() {
         this.attempt--;
         this.updateAttempt();
-    
+
         if (this.attempt < 1) {
             this.handleGameLoss();
         } else {
             soundManager.playError();
         }
     }
-    
+
     handleGameWin() {
         this.gameOver("Congratulations! You guessed the word");
         soundManager.playWin();
     }
-    
+
     handleGameLoss() {
         this.gameOver(`You lost :( </br> The word you were trying to guess was <u>${this.word}</u>. <br/>Let's try again?`);
         soundManager.playLose();
     }
-    
+
     updateAttempt() {
         this.attemptsElement.innerHTML = `${this.attempt}/${MAXIMUM_NUMBERS_OF_ATTEMPTS}`;
         this.attemptDemoElement.setAttribute(
             "src",
             `./images/Hangman-${MAXIMUM_NUMBERS_OF_ATTEMPTS - this.attempt}.png`
         );
-    }    
-
-    openLetter(letter) {
-        const lowercaseLetter = letter.toLowerCase();
-        const guessedWordArray = this.guessedWord.split("");
-    
-        for (let i = 0; i < this.word.length; i++) {
-            if (this.word[i].toLowerCase() === lowercaseLetter) {
-                guessedWordArray[i] = this.word[i];
-            }
-        }
-    
-        this.guessedWord = guessedWordArray.join("");
-        this.wordElement.innerHTML = this.guessedWord;
     }
-    
+
     makeWord() {
         this.word = WORDS[Math.floor(Math.random() * WORDS.length)];
         console.log(this.word);
