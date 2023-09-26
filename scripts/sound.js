1e6e27f3bdbd94884bd51dba52773ffb5b235010
Mikola Kutsko <kn212_kmm@student.ztu.edu.ua>
2023-09-27 01:49:56 +0300

    deleted unused functions

--- scripts/sound.js
+++ scripts/sound.js
@@ -3,26 +3,6 @@ class SoundManager {
         this.sounds = {};
     }
 
-    playSuccess() {
-        this.successSound.currentTime = 0;
-        this.successSound.play();
-    }
-
-    playError() {
-        this.errorSound.currentTime = 0;
-        this.errorSound.play();
-    }
-
-    playWin() {
-        this.winSound.currentTime = 0;
-        this.winSound.play();
-    }
-
-    playLose() {
-        this.loseSound.currentTime = 0;
-        this.loseSound.play();
-    }
-
     loadAndPlaySound(name, src) {
         if (!this.sounds[name]) {
             this.sounds[name] = new Audio(src);
